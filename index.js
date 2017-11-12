a9346de7acb72d29e7ac79ceb89ce3b2a132f94a
Terry Tilley <terry@terrytilley.com>
2017-11-12 00:16:12 +0000

    Sending, Verifying, and Refreshing JWT Tokens in Node.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import path from 'path';
 import cors from 'cors';
+import jwt from 'jsonwebtoken';
 import express from 'express';
 import bodyParser from 'body-parser';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import { makeExecutableSchema } from 'graphql-tools';
 import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
 import models from './models';
+import { refreshTokens } from './auth';
 
 const SECRET = '`sfdgh4kjghfdse22sfghtgytfcesdz38';
 const SECRET2 = '`njkbhgvfcxvbfg34vnbvcfxfd73cgbv1';
@@ -24,20 +26,47 @@ const app = express();
 const graphqlEndpoint = '/graphql';
 
 app.use(cors('*'));
+
+const addUser = async (req, res, next) => {
+  const token = req.headers['x-token'];
+  if (token) {
+    try {
+      const { user } = jwt.verify(token, SECRET);
+      req.user = user;
+    } catch (err) {
+      const refreshToken = req.headers['x-refresh-token'];
+      const newTokens = await refreshTokens(
+        token,
+        refreshToken,
+        models,
+        SECRET,
+        SECRET2
+      );
+      if (newTokens.token && newTokens.refreshToken) {
+        res.set('Access-Control-Expose-Headers', 'x-token, x-refresh-token');
+        res.set('x-token', newTokens.token);
+        res.set('x-refresh-token', newTokens.refreshToken);
+      }
+      req.user = newTokens.user;
+    }
+  }
+  next();
+};
+
+app.use(addUser);
+
 app.use(
   graphqlEndpoint,
   bodyParser.json(),
-  graphqlExpress({
+  graphqlExpress(req => ({
     schema,
     context: {
       models,
-      user: {
-        id: 1,
-      },
+      user: req.user,
       SECRET,
       SECRET2,
     },
-  })
+  }))
 );
 
 app.use('/graphiql', graphiqlExpress({ endpointURL: graphqlEndpoint }));
